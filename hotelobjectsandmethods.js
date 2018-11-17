26a135f6c3f616fcf5d4e4a15e3fa686366b0622
Brett Stainken <brettstainken.student@careerdevs.com>
2018-11-17 17:02:59 +0000

    Nov17MethodPush

---
+++ hotelobjectsandmethods.js
@@ -0,0 +1,21 @@
+console.log("Welcome to the program") //code that is written within the main global scope of a program is executed automatically
+
+var hotel = {
+    name: "CareerDevs Hotel",
+
+    linecook: function (param1,param2){
+    //lines of javascript code
+    //more lines of code
+    //method is a function within an object
+    console .log("Welcome to the Method")
+    }
+}
+console.log("Welcome to the MIDDLE of the program.")
+
+function sousChef(param1,param2){
+    //lines of javascript code
+    //more lines of code
+    console.log("Welcome to the Function")
+}
+
+console.log("Welcome to the END of the program")
