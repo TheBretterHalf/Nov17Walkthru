2b97b222b364a3c7c14c9e5573746d3bc55ae5ae
Brett Stainken <brettstainken.student@careerdevs.com>
2018-11-20 03:01:26 +0000

    Push method

---
+++ hotelobjpush.js
@@ -0,0 +1,59 @@
+var hotel = {
+    name: "CareerDevs Hotel",
+    rating: 5.0,
+    roomrate:325.00,
+    roomNumbers:["101", "102", "103", "104", "105", "106", "107"],
+    roomNumbersBooked:[],
+    roomType: "Queen",
+    roomsavailable: function(roomNumbers, roomNumbersBooked){
+        return this.roomNumbers.length-this.roomNumbersBooked.length;
+    },
+    
+    numberOfRoomsBooked: function() {
+        return this.roomNumbersBooked.length;
+    },
+    
+    numberOfRooms: function() {
+        return this.roomNumber.length + this.roomNumbersBooked.length;
+    },
+    
+    bookAroom: function(roomNumberReq) {
+        if (this.roomsavailable()>0){
+            for (let i=0; i<this.roomNumbers.length; i++){
+                if (this.roomNumbers[i]==roomNumberReq){
+                    this.roomNumbersBooked = this.roomNumbers.splice(i,1).concat(this.roomNumbersBooked);
+                    console.log(this.roomNumbersBooked);
+                    return;
+                }
+                else {
+                    console.log("Not a valid Room #");
+                }
+                        
+            }
+        }
+        else{
+            console.log("No rooms Available");
+        }
+    },
+    bookArandomroom: function(){
+        if (this.roomsavailable()>0){
+            var selectedroom = this.roomNumbers[Math.floor(Math.random()*this.roomNumbers.length)];
+            for (let i=0; i<this.roomNumbers.length; i++){
+                if (this.roomNumbers[i]==selectedroom){
+                    this.roomNumbersBooked = this.roomNumbers.splice(i,1).concat(this.roomNumbersBooked);
+                    console.log(this.roomNumbersBooked);
+                    return;
+                }
+            }
+        } else ("No Rooms Available");
+    },
+    bookArandomroomindex: function(){
+        if (this.roomsavailable()>0){
+            var selectedroom = this.roomNumbers[Math.floor(Math.random()*this.roomNumbers.length)];
+            this.roomNumbers.splice(this.roomNumbers.indexOf(selectedroom),1)
+            this.roomNumbersBooked = this.roomNumbersBooked.push(selectedroom);
+            console.log(this.roomNumbersBooked);
+            return;
+        }
+    }
+};
