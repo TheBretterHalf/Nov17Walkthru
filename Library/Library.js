65d77033f5411a6da8a94408d61ce642e56e97eb
Brett Stainken <brettstainken.student@careerdevs.com>
2018-11-27 17:01:21 +0000

    Library

---
+++ Library/Library.js
@@ -0,0 +1,41 @@
+var Library = {
+    name: "Truth-Brary",
+    books: ["amelia bedilia", "the amber spyglass","fear", "step brothers:the novelization", "coding for dummies"],
+    checkedout:[],
+    booksavail: function (){
+        for (let i=0; i<this.books.length; i++){
+        var CaseBooks = this.books[i].charAt(0).toUpperCase()+this.books[i].slice(1);
+    }
+        return this.CaseBooks
+    },
+    checkoutbook: function(requestedbook){
+        if (this.books.length<0){
+            console.log("Little Town of Footloose")
+        }
+        else
+            var rqbook=requestedbook.toLowerCase();
+            for (let i=0; i>this.books.length; i++){
+                if (rqbook===this.books[i]){
+                    this.checkedout = this.checkedout.concat(this.books.splice(i,1));
+                    return rqbook;
+                }
+                else
+                    console.log("We do not have that book in our Library")
+            }
+    },
+    checkinbook: function(checkbook){
+        if (this.checkedout.length<0){
+            console.log("No Books Checked Out")
+        }
+        else
+            var chkbook = this.checkbook.toLowerCase();
+            for (let i=0; i<this.checkedout.length; i++){
+                if (this.checkbook===this.checkedout[i]){
+                    this.books = this.books.concat(this.checkedout.splice(i,1));
+                    return this.books
+                }
+                else
+                    console.log("You have not checked this book out")
+            }
+    }
+}
